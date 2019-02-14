eca295ed4994f02993efb421e069cbe0d321b584
xcwisc <cxu249@wisc.edu>
2019-02-14 14:18:44 -0600

    Updates

---
+++ precache-manifest.9aa6aaafc61027fc0912faade92a2a9d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "436ca91e9903c2151c268e713b1e85e6",
+    "url": "/static/media/compSci400.436ca91e.png"
+  },
+  {
+    "revision": "874fdcab18392d9defc6",
+    "url": "/static/css/main.ac858d8c.chunk.css"
+  },
+  {
+    "revision": "efdba04681a3215e786c0e996e4bc780",
+    "url": "/static/media/header-background.efdba046.jpg"
+  },
+  {
+    "revision": "5399dccd1051fe4c247d",
+    "url": "/static/js/1.5399dccd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5ed4830678ff510566db8eaefb28951e",
+    "url": "/static/media/ece252.5ed48306.png"
+  },
+  {
+    "revision": "874fdcab18392d9defc6",
+    "url": "/static/js/main.874fdcab.chunk.js"
+  },
+  {
+    "revision": "54ef290956f47182fc582806e492db81",
+    "url": "/static/media/ai.54ef2909.png"
+  },
+  {
+    "revision": "5097b0247a92d47178df598b82944f15",
+    "url": "/static/media/default-avatar.5097b024.png"
+  },
+  {
+    "revision": "070ccffe9485bdf1299bc3b5f501202b",
+    "url": "/static/media/foodQueryCover.070ccffe.PNG"
+  },
+  {
+    "revision": "5399dccd1051fe4c247d",
+    "url": "/static/css/1.5724e707.chunk.css"
+  },
+  {
+    "revision": "1fbdc5638d23c751166f982b858cd53d",
+    "url": "/index.html"
+  }
+];
